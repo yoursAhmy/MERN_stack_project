50766ded09b6dc184b98b814e24a15935f484792
Ahmad Qaisar <ahmadqaisar244623@gmail.com>
2025-08-27 21:16:52 +0500

    Utils implementation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import  dotenv from "dotenv"
 import connectDB from './db/index.js';
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
-connectDB();
-
-
-
-
-
-
-
-
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("server is running at port:", process.env.PORT);
+    })
+})
+.catch((err) => {
+    console.log("MobgoDB connnection failed",err );
+    
+})
 
 
 // const app = express()
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asynchandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+};
+
+export {asynchandler}
